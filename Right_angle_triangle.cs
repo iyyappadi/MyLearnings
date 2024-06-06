44fb81248cd16cd9a02c433b4bb595236e4b1812
iyyappadi <siyyappadi@gmail.com>
2024-06-06 10:41:18 +0530

    Completed printing right angle triangle program

---
+++ Right_angle_triangle.cs
@@ -0,0 +1,25 @@
+﻿namespace Training;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        for (int i = 1; i <= 5; i++) {
+
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("#");
+
+            }
+            
+            Console.WriteLine("");
+          
+
+        }
+     
+       
+    }
+}
+
+
