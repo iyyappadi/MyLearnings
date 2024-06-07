0dc37984ef41cb71e7fbf9851fd9a02f99be99c1
iyyappadi <siyyappadi@gmail.com>
2024-06-07 11:33:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Training;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number to find the Factorial");
+        int number = int.Parse(Console.ReadLine());
+         for (int i = number; i <= 1; i--)
+        { 
+            Console.WriteLine(i);
+        }
+    }
+    
+
+
+}
+     
+       
+    
+
+
+
